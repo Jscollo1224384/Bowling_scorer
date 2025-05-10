f95add988d30d2dbfe2067db88a96bb684eb751b
Joseph Scollo <joseph.scollo@payroc.com>
2025-05-09 22:34:12 -0400

    added test to suite

--- test_cases.c
+++ test_cases.c
@@ -15,9 +15,25 @@ void tearDown(void)
 }
 
 /* Test Suite */
+
+void test_a_game_has_ten_frames(void)
+{
+    int expected_frames = 10;
+    game_t *game = create_game();
+    if (game == NULL) {
+        TEST_FAIL_MESSAGE("Failed to create game");
+    }
+    int actual_frames = 0;
+    for (int i = 0; i < NUM_OF_FRAMES; i++) {
+        if (game->frames[i] != NULL) {
+            actual_frames++;
+        }
+    }
+    TEST_ASSERT_EQUAL(expected_frames, actual_frames);
+}
 void run_tests(void)
 {
     UnityBegin("Running Tests");
-
+    RUN_TEST(test_a_game_has_ten_frames);
     UnityEnd();
 }
