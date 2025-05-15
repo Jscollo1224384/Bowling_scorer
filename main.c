49a2a012a670cb080a7b153fae50a302db31bc53
Joseph Scollo <joseph.scollo@payroc.com>
2025-05-15 16:24:37 -0400

    added sample game sheets 2d arrays

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "test_cases.h"
 #include "Unity-2.6.1/src/unity.h"
+#include "game_attributes.h"
 
 int main(int argc, char *argv[]) {
     if (argc > 1 && strcmp(argv[1], "debug") == 0) {
@@ -10,6 +11,32 @@ int main(int argc, char *argv[]) {
     } else {
         printf("Running in normal mode.\n");
     }
+    int rolls_perfect_game[10][3] = {
+        {10, 0, 0}, // Frame 1: Strike
+        {10, 0, 0}, // Frame 2: Strike
+        {10, 0, 0}, // Frame 3: Strike
+        {10, 0, 0}, // Frame 4: Strike
+        {10, 0, 0}, // Frame 5: Strike
+        {10, 0, 0}, // Frame 6: Strike
+        {10, 0, 0}, // Frame 7: Strike
+        {10, 0, 0}, // Frame 8: Strike
+        {10, 0, 0}, // Frame 9: Strike
+        {10, 10, 10} // Frame 10: Strike + bonus rolls
+    };
+    int rolls_game[10][3] = {
+        {10, 0, 0}, // Frame 1: Strike
+        {7, 3, 0}, // Frame 2: Spare
+        {9, 0, 0}, // Frame 3: Open frame
+        {10, 0, 0}, // Frame 4: Strike
+        {10, 0, 0}, // Frame 5: Strike
+        {10, 0, 0}, // Frame 6: Strike
+        {10, 0, 0}, // Frame 7: Strike
+        {10, 0, 0}, // Frame 8: Strike
+        {10, 0, 0}, // Frame 9: Strike
+        {10, 10, 10} // Frame 10: Strike + bonus rolls
+    };
+    game_t *game = create_game();
+    record_frames(game, rolls_game);
 
     return 0;
 }
